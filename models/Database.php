8a9ba880b782a71225d383b48676640e2ebda5aa
tigerhostsolutions <jgrasty@tigerhost.domains>
2025-02-28 17:14:01 -0500

    updated: updated

--- models/Database.php
+++ models/Database.php
@@ -3,11 +3,13 @@
     
     use Dotenv\Dotenv;
     
-    class Database {
+    class Database
+    {
         private static $pdo;
         
-        public static function getConnection() {
-            if (self::$pdo === null) {
+        public static function getConnection()
+        {
+            if (self::$pdo === NULL) {
                 // Determine which environment file to load
                 $env = getenv('APP_ENV') ?: 'development'; // Defaults to 'development' if APP_ENV is not set
                 $envFile = $env === 'production' ? '.env-production' : '.env';
@@ -19,18 +21,18 @@
                 // Retrieve values from the .env file
                 $host = $_ENV['DB_HOST'] ?? 'localhost';
                 $db = $_ENV['DB_NAME'] ?? 'twitter_clone';
-                $user = $_ENV['DB_USER'] ?? 'badtwitter';
+                $username = $_ENV['DB_USER'] ?? 'badtwitter';
                 $password = $_ENV['DB_PASSWORD'] ?? 'badtwitter';
+                $dsn = "mysql:host={$host};dbname={$db}";
                 
                 // RDS connection
-                $dbhost = $_SERVER['RDS_HOSTNAME'];
-                $dbport = $_SERVER['RDS_PORT'];
-                $dbname = $_SERVER['RDS_DB_NAME'];
-                $charset = 'utf8';
-                
-                $dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
-                $username = $_SERVER['RDS_USERNAME'];
-                $password = $_SERVER['RDS_PASSWORD'];
+//                $dbhost = $_SERVER['RDS_HOSTNAME'];
+//                $dbport = $_SERVER['RDS_PORT'];
+//                $dbname = $_SERVER['RDS_DB_NAME'];
+//                $charset = 'utf8';
+//                $dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
+//                $username = $_SERVER['RDS_USERNAME'];
+//                $password = $_SERVER['RDS_PASSWORD'];
                 
                 try {
                     self::$pdo = new PDO($dsn, $username, $password);
