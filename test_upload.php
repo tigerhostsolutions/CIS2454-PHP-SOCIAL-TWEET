ac014d1dd951f5332bde138d624abe0415d1bf0f
tigerhostsolutions <jgrasty@tigerhost.domains>
2025-03-02 13:01:39 -0500

    untracked files

--- test_upload.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $uploadsDir = "E:/path/to/uploads"; // Update the path based on your setup
-    if (is_writable($uploadsDir)) {
-        echo "Uploads folder is writable!";
-    } else {
-        echo "Uploads folder is not writable.";
-    }
-?>
