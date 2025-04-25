bb64fc9dd3699a1f5494295ba03a0d4aca3a1c09
tigerhostsolutions <jgrasty@tigerhost.domains>
2025-04-24 23:06:38 -0400

    Include Qodana configuration file

--- profile.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // profile.php
-    // This file handles the profile page of the application.
-    use App\Controllers\UserController;
-    
-    require_once __DIR__ . '/config.php';
-    require_once MODEL_PATH . 'Database.php';
-    require_once CONTROLLER_PATH . 'UserController.php';
-    UserController::handleRequest();
